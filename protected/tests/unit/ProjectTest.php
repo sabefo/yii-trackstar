4051ebf548cf0b716fcaf4273bb5eb16e09a2614
Santiago Bermudez Fortes <sbermudezf@elementia.com>
2017-06-01 09:13:26 -0500

    Project's CRUD tests refactored

--- protected/tests/unit/ProjectTest.php
+++ protected/tests/unit/ProjectTest.php
@@ -4,45 +4,44 @@
 			'projects' => 'Project',
 		);
 
+		public function testCreate() {
+			$newProject = new Project;
+			$newProjectName = 'Test Project Creation';
+			$newProject -> setAttributes(array(
+				'name' => $newProjectName,
+				'description' => 'This is a test for new project creation',
+				'createTime' => '2009-09-09 00:00:00',
+				'createUser' => '1',
+				'updateTime' => '2009-09-09 00:00:00',
+				'updateUser' => '1',
+			));
+			$this -> assertTrue($newProject -> save(false));
+
+			$retrievedProject = Project::model() -> findByPk($newProject -> id);
+			$this -> assertTrue($retrievedProject instanceof Project);
+			$this -> assertEquals($newProjectName, $retrievedProject -> name);
+		}
+
 		public function testRead() {
 			$retrievedProject = $this -> projects('project1');
 			$this -> assertTrue($retrievedProject instanceof Project);
 			$this -> assertEquals('Test Project 1', $retrievedProject -> name);
 		}
 
-		public function testCRUD() {
-			$newProject = new Project;
-			$newProjectName = 'Test Project 1';
-			$newProject -> setAttributes(
-				array(
-					'name' => $newProjectName,
-					'description' => 'Test project number one',
-					'create_time' => '2010-01-01 00:00:00',
-					'create_user_id' => 1,
-					'update_time' => '2010-01-01 00:00:00',
-					'update_user_id' => 1,
-				)
-			);
-			$this -> assertTrue($newProject -> save(false));
-
-			//READ back the newly created project
-			$retrievedProject = Project::model() -> findByPk($newProject -> id);
+		public function testUpdate() {
+			$retrievedProject = $this -> projects('project1');
 			$this -> assertTrue($retrievedProject instanceof Project);
-			$this -> assertEquals($newProjectName, $retrievedProject -> name);
+			$newName = 'Test project 1 new name';
+			$retrievedProject -> name = $newName;
+			$this -> assertTrue($retrievedProject -> save(false));
+			$this -> assertEquals($retrievedProject -> name, $newName);
+		}
 
-			//UPDATE the newly created project
-			$updatedProjectName = 'Updated Test Project 1';
-			$newProject -> name = $updatedProjectName;
-			$this -> assertTrue($newProject -> save(false));
-			//read back the record again to ensure the update worked
-			$updatedProject = Project::model() -> findByPk($newProject -> id);
-			$this -> assertTrue($updatedProject instanceof Project);
-			$this -> assertEquals($updatedProjectName, $updatedProject -> name);
-
-			//DELETE the project
-			$newProjectId = $newProject -> id;
-			$this -> assertTrue($newProject -> delete());
-			$deletedProject = Project::model() -> findByPk($newProjectId);
+		public function testDelete() {
+			$retrievedProject = $this -> projects('project1');
+			$projectId = $retrievedProject -> id;
+			$this -> assertTrue($retrievedProject -> delete());
+			$deletedProject = Project::model() -> findByPk($projectId);
 			$this -> assertEquals(NULL, $deletedProject);
 		}
 	}
