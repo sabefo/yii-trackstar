a845e61c82cc0b21ec6839519197fe8552470f9e
Santiago Bermudez Fortes <sbermudezf@elementia.com>
2017-06-05 16:14:59 -0500

    added rbac shell command

--- sql_statements.sql
+++ sql_statements.sql
@@ -92,3 +92,13 @@ create table AuthAssignment (
 	primary key (itemname,userid),
 	foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
 );
+
+create table tbl_project_user_role (
+	project_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	role VARCHAR(64) NOT NULL,
+	primary key (project_id, user_id, role),
+	foreign key (project_id) references tbl_project (id),
+	foreign key (user_id) references tbl_user (id),
+	foreign key (role) references AuthItem (name)
+);
