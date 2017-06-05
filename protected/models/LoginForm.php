f6d412d5ce41e5f47524aff169b27db0b9538fb6
Santiago Bermudez Fortes <sbermudezf@elementia.com>
2017-06-05 12:28:27 -0500

    added last login info in homepage

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -68,7 +68,7 @@ class LoginForm extends CFormModel
         if ($this -> _identity -> errorCode === UserIdentity::ERROR_NONE) {
             $duration = $this -> rememberMe ? 3600 * 24 * 30 : 0; // 30 days
             Yii::app() -> user -> login($this -> _identity, $duration);
-			User::model() -> updateByPk($this -> _identity -> id, array('last_login_time' => new CDbExpresion('NOW()')));
+			User::model() -> updateByPk($this -> _identity -> id, array('last_login_time' =>  date('Y-m-d H:i:s')));
             return true;
         } else {
             return false;
