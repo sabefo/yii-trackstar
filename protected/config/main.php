45a73131ab05fe706c1b33eb55a3a14a6b36dca3
Santiago Bermudez Fortes <sbermudezf@elementia.com>
2017-06-07 11:15:53 -0500

    created new theme

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,8 +6,10 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name' => 'TrackStar',
+	'homeUrl' => '/trakstar/project',
+	'theme' => 'new',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- themes/new/views/main.php
+++ themes/new/views/main.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app() -> theme -> baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
@@ -23,7 +23,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo"></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
