353ba10e323e436dcd3c209f150936229a276788
Santiago Bermudez Fortes <sbermudezf@elementia.com>
2017-06-20 17:25:16 -0500

    few syntax changes

--- protected/tests/unit/ProjectTest.php
+++ protected/tests/unit/ProjectTest.php
@@ -3,73 +3,73 @@ class ProjectTest extends CDbTestCase
 {
 
     public $fixtures = array(
-		'projects' => 'Project',
-		'users' => 'User',
-		'projUsrAssign' => ':tbl_project_user_assignment',
-		'projUserRole' => ':tbl_project_user_role',
-		'authAssign' => ':AuthAssignment',
+  		'projects' => 'Project',
+  		'users' => 'User',
+  		'projUsrAssign' => ':tbl_project_user_assignment',
+  		'projUserRole' => ':tbl_project_user_role',
+  		'authAssign' => ':AuthAssignment',
     );
 
     public function testCreate()
     {
-        $newProject = new Project;
-        $newProjectName = 'Test Project Creation';
-        $newProject -> setAttributes(
-            array(
-	            'name' => $newProjectName,
-	            'description' => 'This is a test for new project creation',
-            )
-        );
-        Yii::app() -> user -> setId($this -> users('user1') -> id);
-        $this -> assertTrue($newProject -> save());
+      $newProject = new Project;
+      $newProjectName = 'Test Project Creation';
+      $newProject -> setAttributes(
+          array(
+            'name' => $newProjectName,
+            'description' => 'This is a test for new project creation',
+          )
+      );
+      Yii::app() -> user -> setId($this -> users('user1') -> id);
+      $this -> assertTrue($newProject -> save());
 
-        $retrievedProject = Project::model() -> findByPk($newProject -> id);
-        // echo $retrievedProject -> create_user_id;die;
-        $this -> assertTrue($retrievedProject instanceof Project);
-        $this -> assertEquals($newProjectName, $retrievedProject -> name);
-        $this -> assertEquals(Yii::app() -> user -> id, $retrievedProject -> create_user_id);
+      $retrievedProject = Project::model() -> findByPk($newProject -> id);
+      // echo $retrievedProject -> create_user_id;die;
+      $this -> assertTrue($retrievedProject instanceof Project);
+      $this -> assertEquals($newProjectName, $retrievedProject -> name);
+      $this -> assertEquals(Yii::app() -> user -> id, $retrievedProject -> create_user_id);
     }
 
     public function testRead()
     {
-        $retrievedProject = $this -> projects('project1');
-        $this -> assertTrue($retrievedProject instanceof Project);
-        $this -> assertEquals('Test Project 1', $retrievedProject -> name);
+      $retrievedProject = $this -> projects('project1');
+      $this -> assertTrue($retrievedProject instanceof Project);
+      $this -> assertEquals('Test Project 1', $retrievedProject -> name);
     }
 
     public function testUpdate()
     {
-        $retrievedProject = $this -> projects('project1');
-        $this -> assertTrue($retrievedProject instanceof Project);
-        $newName = 'Test project 1 new name';
-        $retrievedProject -> name = $newName;
-        $this -> assertTrue($retrievedProject -> save(false));
-        $this -> assertEquals($retrievedProject -> name, $newName);
+      $retrievedProject = $this -> projects('project1');
+      $this -> assertTrue($retrievedProject instanceof Project);
+      $newName = 'Test project 1 new name';
+      $retrievedProject -> name = $newName;
+      $this -> assertTrue($retrievedProject -> save(false));
+      $this -> assertEquals($retrievedProject -> name, $newName);
     }
 
     public function testDelete()
     {
-        $retrievedProject = $this -> projects('project1');
-        $projectId = $retrievedProject -> id;
-        $this -> assertTrue($retrievedProject -> delete());
-        $deletedProject = Project::model() -> findByPk($projectId);
-        $this -> assertEquals(null, $deletedProject);
+      $retrievedProject = $this -> projects('project1');
+      $projectId = $retrievedProject -> id;
+      $this -> assertTrue($retrievedProject -> delete());
+      $deletedProject = Project::model() -> findByPk($projectId);
+      $this -> assertEquals(null, $deletedProject);
     }
 
     public function testGetUserOptions()
     {
-        $project = $this -> projects('project2');
-        $options = $project -> userOptions;
-        $this -> assertTrue(is_array($options));
-        $this -> assertTrue(count($options) > 0);
+      $project = $this -> projects('project2');
+      $options = $project -> userOptions;
+      $this -> assertTrue(is_array($options));
+      $this -> assertTrue(count($options) > 0);
     }
 
     public function testUserRoleAssignment()
     {
-        $project = $this -> projects('project1');
-        $user = $this -> users('user1');
-        $this -> assertEquals(1, $project -> associateUserToRole('owner', $user -> id));
-		$this -> assertEquals(1, $project -> removeUserFromRole('owner', $user -> id));
+      $project = $this -> projects('project1');
+      $user = $this -> users('user1');
+      $this -> assertEquals(1, $project -> associateUserToRole('owner', $user -> id));
+	    $this -> assertEquals(1, $project -> removeUserFromRole('owner', $user -> id));
     }
 
 	public function testIsInRole()
--- sql_statements.sql
+++ sql_statements.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tbl_project (
+CREATE TABLE IF NOT EXISTS tbl_project (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(128),
 	description TEXT,
@@ -44,31 +44,23 @@ CREATE TABLE IF NOT EXISTS tbl_project_user_assignment (
 	update_user_id INTEGER,
 	PRIMARY KEY (project_id, user_id)
 ) ENGINE = InnoDB;
+
 -- The Relationships
-ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_project FOREIGN KEY
-(project_id) REFERENCES tbl_project (id) ON DELETE CASCADE ON
-UPDATE RESTRICT;
-ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_owner FOREIGN KEY
-(owner_id) REFERENCES tbl_user (id) ON DELETE CASCADE ON UPDATE
-RESTRICT;
-ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_requester FOREIGN
-KEY (requester_id) REFERENCES tbl_user (id) ON DELETE CASCADE ON
-UPDATE RESTRICT;
-ALTER TABLE tbl_project_user_assignment ADD CONSTRAINT FK_project_user FOREIGN KEY (project_id) REFERENCES tbl_project (id) ON
-DELETE CASCADE ON UPDATE RESTRICT;
-ALTER TABLE tbl_project_user_assignment ADD CONSTRAINT FK_user_project FOREIGN KEY (user_id) REFERENCES tbl_user (id) ON
-DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_project FOREIGN KEY (project_id) REFERENCES tbl_project (id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_owner FOREIGN KEY (owner_id) REFERENCES tbl_user (id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE tbl_issue ADD CONSTRAINT FK_issue_requester FOREIGN KEY (requester_id) REFERENCES tbl_user (id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE tbl_project_user_assignment ADD CONSTRAINT FK_project_user FOREIGN KEY (project_id) REFERENCES tbl_project (id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE tbl_project_user_assignment ADD CONSTRAINT FK_user_project FOREIGN KEY (user_id) REFERENCES tbl_user (id) ON DELETE CASCADE ON UPDATE RESTRICT;
 -- Insert some seed data so we can just begin using the database
-INSERT INTO tbl_user (email, username, password)
-VALUES
+INSERT INTO tbl_user (email, username, password) VALUES
 ('test1@notanaddress.com','Test_User_One', MD5('test1')),
-('test2@notanaddress.com','Test_User_Two', MD5('test2'));
+('test2@notanaddress.com','Test_User_Two', MD5('test2')),
 ('test3@notanaddress.com','Test_User_Three', MD5('test3'));
-INSERT INTO tbl_project_user_assignment (project_id, user_id, create_time, create_user_id, update_time, update_user_id)
-VALUES (2, 1, '2010-01-01 00:00:00', 1, '2010-01-01 00:00:00', 1);
+INSERT INTO tbl_project_user_assignment (project_id, user_id, create_time, create_user_id, update_time, update_user_id) VALUES
+-- (2, 1, '2010-01-01 00:00:00', 1, '2010-01-01 00:00:00', 1),
 (2, 2, '2010-01-01 00:00:00', 1, '2010-01-01 00:00:00', 1);
 
-create table AuthItem (
+CREATE TABLE IF NOT EXISTS AuthItem (
 	name varchar(64) not null,
 	type integer not null,
 	description text,
@@ -77,7 +69,7 @@ create table AuthItem (
 	primary key (name)
 );
 
-create table AuthItemChild (
+CREATE TABLE IF NOT EXISTS AuthItemChild (
 	parent varchar(64) not null,
 	child varchar(64) not null,
 	primary key (parent,child),
@@ -85,7 +77,7 @@ create table AuthItemChild (
 	foreign key (child) references AuthItem (name) on delete cascade on update cascade
 );
 
-create table AuthAssignment (
+CREATE TABLE IF NOT EXISTS AuthAssignment (
 	itemname varchar(64) not null,
 	userid varchar(64) not null,
 	bizrule text,
@@ -94,7 +86,7 @@ create table AuthAssignment (
 	foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
 );
 
-create table tbl_project_user_role (
+CREATE TABLE IF NOT EXISTS tbl_project_user_role (
 	project_id INTEGER NOT NULL,
 	user_id INTEGER NOT NULL,
 	role VARCHAR(64) NOT NULL,
@@ -104,7 +96,7 @@ create table tbl_project_user_role (
 	foreign key (role) references AuthItem (name)
 );
 
-CREATE TABLE tbl_comment (
+CREATE TABLE IF NOT EXISTS tbl_comment (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	content TEXT NOT NULL,
 	issue_id INTEGER,
@@ -114,12 +106,10 @@ CREATE TABLE tbl_comment (
 	update_user_id INTEGER
 );
 
-ALTER TABLE tbl_comment ADD CONSTRAINT FK_comment_issue FOREIGN
-KEY (issue_id) REFERENCES tbl_issue (id);
-ALTER TABLE tbl_comment ADD CONSTRAINT FK_comment_author FOREIGN
-KEY (create_user_id) REFERENCES tbl_user (id);
+ALTER TABLE tbl_comment ADD CONSTRAINT FK_comment_issue FOREIGN KEY (issue_id) REFERENCES tbl_issue (id);
+ALTER TABLE tbl_comment ADD CONSTRAINT FK_comment_author FOREIGN KEY (create_user_id) REFERENCES tbl_user (id);
 
-CREATE TABLE tbl_sys_message (
+CREATE TABLE IF NOT EXISTS tbl_sys_message (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	message TEXT NOT NULL,
 	create_time DATETIME,
